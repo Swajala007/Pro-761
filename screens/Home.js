a92c86e4cdba6beaf8a7a0024b748e27d33d5599
Swajala007 <86418960+Swajala007@users.noreply.github.com>
2022-03-30 15:09:42 +0530

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor:"black"
+                    
+                }}>
+                <Text style={{
+                    color:"blue",
+                    fontSize:25,
+                    fontStyle:"italic"
+                }}> :D ~`~`~** : ) Home Screen ( :**~`~`~ C:</Text>
+
+<Text style={{
+                    color:"lightgreen",
+                    fontSize:25,
+                    fontStyle:"italic"
+                }}>Welcome!!
+                </Text>
+
+                <Text style={{
+                    color:"pink",
+                    fontSize:25,
+                    fontStyle:"italic"
+                }}>Here you can get all the informations....c:
+                </Text>
+
+                <Text style={{
+                    color:"lightblue",
+                    fontSize:25,
+                    fontStyle:"italic",
+                    marginTop:100
+                }}>Credit : "Jeff Sir"
+                </Text>
+
+                <Text style={{
+                    color:"purple",
+                    fontSize:25,
+                    fontStyle:"italic",
+                   
+                }}>Want to join us online?
+                </Text>
+
+                <Text style={{
+                    color:"orange",
+                    fontSize:25,
+                    fontStyle:"italic",
+                   
+                }}>no.3378945201
+                </Text>
+
+                <Text style={{
+                    color:"yellow",
+                    fontSize:25,
+                    fontStyle:"italic",
+                   marginTop:80
+                }}>If you like our website,please contact us with this no. We will tell you all
+                informations and steps to join us through online in 
+                details.We hope you will learn more and more new things
+                from our Top-Best Teacher-Jeff Sir.We will be happy to help you to
+                learn more new things.
+                We are available 24hrs for providing 
+                you the best service.Please feel free to contact us.
+                We will give our best to provide you a ocean of knowledge
+                and make you stand in the Top-1.
+                Hope you like our website.
+                </Text>
+
+                <Text style={{
+                    color:"red",
+                    fontSize:25,
+                    fontStyle:"italic",
+                    marginTop:20
+                }}>'u'..* Happy Learning*..'u'
+                </Text>
+
+            </View>
+        )
+    }
+}
